4eae52b5410a8b385671566d6424f0a21b76cd3f
Ionut Grigore <ionut.orlando@yahoo.com>
2020-06-12 19:44:10 +0300

    GEEK-6 Implementare simpla pentru afisare comenzi anterioare

---
+++ src/main/java/Controllers/PrevOrdersController.java
@@ -0,0 +1,56 @@
+package Controllers;
+
+import Models.Order;
+import Models.Product;
+import Services.OrderServices;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+public class PrevOrdersController {
+    @FXML
+    private TableView<Order> tableView;
+    @FXML
+    private TableColumn<Order,String> tableColumn;
+
+    @FXML
+    public void initialize(){
+        ArrayList<Order> orders = OrderServices.getOrders();
+        ArrayList<Order> sortedOrders = new ArrayList<>();
+
+        for(Order o:orders){
+            if(o.getId().equals(LoginController.getId())){
+                sortedOrders.add(o);
+            }
+        }
+
+        tableView.setItems(FXCollections.observableArrayList(sortedOrders));
+        tableColumn.setCellValueFactory(new PropertyValueFactory<Order,String>("product"));
+    }
+
+    @FXML
+    public void backButton(){
+        try{
+            Stage primaryStage = (Stage)tableView.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("shopPage.fxml"));
+            primaryStage.setTitle("Shop Page");
+            primaryStage.setScene(new Scene(root,600,500));
+            primaryStage.show();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    public void showButton(){
+
+    }
+}
